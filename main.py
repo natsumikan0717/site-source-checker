0c38886e15b47858376422e2201681e50c3c0a5c
natsumikan0717 <natsumikan.0717@gmail.com>
2026-04-18 20:05:04 +0900

    コメントアウトのメッセージ修正

--- main.py
+++ main.py
@@ -11,20 +11,23 @@ dt = now.date().strftime('%Y%m%d')
 # 1. 取得したいURLを指定
 page = "https://www.google.com/"
 name = "google"
+
+# 2. 取得したいURLを指定
 try:
-    # 2. ページ内容を取得
+    # 2-1. ページ内容を取得
     response = requests.get(page)
 
-    # ステータスコードが200（成功）か確認
+    # 2-2. ステータスコードが200（成功）か確認
     response.raise_for_status()
 
-    # 3. テキストファイルに書き出し（文字化け防止のためutf-8を指定）
+    # 2-3. テキストファイルに書き出し（文字化け防止のためutf-8を指定）
     # filename = 'pagename_' + now.strftime('%Y%m%d') + '.txt'
     # filename = './output/' + name + '_' + now.strftime('%Y%m%d') + '.txt'
     filename = name + '_' + now.strftime('%Y%m%d') + '.txt'
     with open(filename, "w", encoding="utf-8") as f:
         f.write(response.text)
 
+    # 2-4. 成功ならメッセージを出力する
     print("ファイルへの書き出しが完了しました。")
     print(dt)
 
